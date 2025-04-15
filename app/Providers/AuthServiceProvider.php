c588e3aaf9fec567d69127cbb096beabf479d0c6
ishimwerosette7 <ishimwerosette7@gmail.com>
2025-04-15 08:50:14 +0200

    Update AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace App\Providers;
-
 use App\Models\Product;
 use App\Policies\ProductPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
 class AuthServiceProvider extends ServiceProvider
 {
     /**
