4977e2472d1cd62afcf701b31cf46aa195671df4
ishimwerosette7 <ishimwerosette7@gmail.com>
2025-04-15 08:45:40 +0200

    Update User

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,10 @@
 <?php
-
 namespace App\Models;
-
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
@@ -28,7 +25,6 @@ class User extends Authenticatable
         'business_type',
         'password',
     ];
-
     /**
      * The attributes that should be hidden for serialization.
      *
