ddd52143cb70a8b95ba09b76b3620443302468a7
ishimwerosette7 <ishimwerosette7@gmail.com>
2025-04-15 09:16:28 +0200

    Update dashboard.blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -45,7 +45,6 @@
             </div>
         </div>
     </nav>
-
     <div class="container mt-4">
         <div class="row">
             <div class="col-md-12">
@@ -106,7 +105,6 @@
             </div>
         </div>
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html> 
