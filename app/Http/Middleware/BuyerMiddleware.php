ac5459dd5aaadfed2fe51a53b626e94f19e83057
ishimwerosette7 <ishimwerosette7@gmail.com>
2025-04-15 02:39:13 +0200

    Update BuyerMiddleware

--- app/Http/Middleware/BuyerMiddleware.php
+++ app/Http/Middleware/BuyerMiddleware.php
@@ -1,12 +1,9 @@
 <?php
-
 namespace App\Http\Middleware;
-
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
-
 class BuyerMiddleware
 {
     /**
