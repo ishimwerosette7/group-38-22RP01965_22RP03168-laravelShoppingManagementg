61581a931823b0b0efc522b14cf560aba301e5df
ishimwerosette7 <ishimwerosette7@gmail.com>
2025-04-15 08:56:55 +0200

    Update ProductSeeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 use App\Models\Product;
 use App\Models\User;
-
 class ProductSeeder extends Seeder
 {
     public function run(): void
