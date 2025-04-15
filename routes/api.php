2aa6f6622c22d84fd1e1ce885198e7d733c1a766
TWIZERIMANAHEN <twizerimanahenrette@gmail.com>
2025-04-15 08:42:29 +0200

    Update api

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\UserActivityController;
+
 // Public routes
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
